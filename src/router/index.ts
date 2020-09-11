66bd530fbdd55dbb3d9615a86c73871a1cd15f35
bibi1989 <bibiaremieye@gmail.com>
2020-09-11 08:57:45 +0100

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -58,6 +58,7 @@ const routes: Array<RouteConfig> = [
   //   name: "About",
   //   component: () =>
   //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // making a change
   // }
 ];
 
